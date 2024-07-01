3d29cea7c2ab4326678c7fda22d0760dff8a909e
easygo-dev <85024078+easygo-dev@users.noreply.github.com>
2024-07-01 15:19:11 +0300

    Update create_node_checker.sh

--- create_node_checker.sh
+++ create_node_checker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 cat << "EOF"
+
 Processing...
+
 EOF
 
 sleep 5 # add sleep time
@@ -48,55 +50,54 @@ echo "Creating or overwriting script..."
 sleep 1
 cat << EOF >| $SCRIPT_FILE
 #!/bin/bash
+
 # check with that cmd
 CHECK_COMMAND="$GRPCURL_PATH -plaintext localhost:8337 quilibrium.node.node.pb.NodeService.GetNetworkInfo"
 
 # log
 LOG_DIR=/root/scripts/log
-LOG_FILE=\$LOG_DIR/node_check.log
+LOG_FILE=$LOG_DIR/node_check.log
 MAX_LOG_SIZE=10240
 
 # rotate
 rotate_logs() {
-    if [ -f "\$LOG_FILE" ]; then
-        local log_size_kb=\$(du -k "\$LOG_FILE" | cut -f1)
-        if [ "\$log_size_kb" -ge "\$MAX_LOG_SIZE" ]; then
-            mv "\$LOG_FILE" "\$LOG_FILE.1"
-            touch "\$LOG_FILE"
+    if [ -f "$LOG_FILE" ]; then
+        local log_size_kb=$(du -k "$LOG_FILE" | cut -f1)
+        if [ "$log_size_kb" -ge "$MAX_LOG_SIZE" ]; then
+            mv "$LOG_FILE" "$LOG_FILE.1"
+            touch "$LOG_FILE"
         fi
     fi
 }
 
 # making dir
-mkdir -p \$LOG_DIR
-
-
+mkdir -p $LOG_DIR
 
 # rotate
 rotate_logs
 
 # command
-output=\$($CHECK_COMMAND 2>&1)
+output=$($CHECK_COMMAND 2>&1)
 
 # log
-timestamp=\$(date '+%Y-%m-%d %H:%M:%S')
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+echo "$timestamp - Output from command:" | tee -a $LOG_FILE
+echo "$output" | tee -a $LOG_FILE
 
 # check errors in result
-if echo "\$output" | grep -q "Failed to dial target host"; then
-    echo "\$timestamp - Error detected: restarting node" | tee -a \$LOG_FILE
-    sudo service ceremonyclient restart | tee -a \$LOG_FILE
-    if [ \$? -ne 0 ]; then
-        echo "\$timestamp - Failed to restart ceremonyclient service." | tee -a \$LOG_FILE
+if echo "$output" | grep -q "Failed to dial target host"; then
+    echo "$timestamp - Error detected: restarting node" | tee -a $LOG_FILE
+    sudo service ceremonyclient restart | tee -a $LOG_FILE
+    if [ $? -ne 0 ]; then
+        echo "$timestamp - Failed to restart ceremonyclient service." | tee -a $LOG_FILE
     else
-        echo "\$timestamp - Ceremonyclient service restarted successfully." | tee -a \$LOG_FILE
+        echo "$timestamp - Ceremonyclient service restarted successfully." | tee -a $LOG_FILE
     fi
 else
-    echo "\$timestamp - Node is running correctly" | tee -a \$LOG_FILE
+    echo "$timestamp - Node is running correctly" | tee -a $LOG_FILE
 fi
-
 EOF
 check_command "Failed to create or overwrite monitoring script"
-
 
 # Make the script executable
 echo "Making the script executable..."
@@ -123,16 +124,9 @@ else
     fi
 fi
 
-
-
 echo "Installation complete. The monitoring script has been set up and the cron job has been created or skipped."
-
 sleep 1
-
 echo "You can find the monitoring script at: $SCRIPT_FILE"
-
 sleep 1
-
 echo "Logs will be written to: ~/scripts/log/"
-
 sleep 1
